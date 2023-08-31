2abd756cf9dd544e6ac9b67e69328291e6243e68
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-31 14:54:31 +0100

    Finish express migration

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,22 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World');
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, '../html/about.html'));
+});
+
+app.get('/contact-me', (req, res) => {
+  res.sendFile(path.join(__dirname, '../html/contact-me.html'));
+});
+
+app.use(function (req, res) {
+  res.status(404).sendFile(path.join(__dirname, '../html/404.html'));
 });
 
 app.listen(port, () => {
