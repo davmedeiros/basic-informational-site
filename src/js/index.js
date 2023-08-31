638fa72f040dcd409772a8960f1d7bceacb79833
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-31 14:42:31 +0100

    Start reimplementing the app with express

--- src/js/index.js
+++ src/js/index.js
@@ -1,42 +1,11 @@
-const http = require('http');
-const fs = require('fs/promises');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const serve = async (page) => {
-  try {
-    await http
-      .createServer(async (req, res) => {
-        let pathname = await '';
+app.get('/', (req, res) => {
+  res.send('Hello, World');
+});
 
-        if (req.url === '/') {
-          pathname = await `./src/index.html`;
-        } else {
-          pathname = await `./src/html/${req.url}.html`;
-        }
-
-        let data = await '';
-
-        try {
-          data = await fs.readFile(pathname, {
-            encoding: 'utf8',
-          });
-          res.writeHead(200, { 'Content-Type': 'text/html' });
-        } catch (error) {
-          data = await fs.readFile('./src/html/404.html', {
-            encoding: 'utf8',
-          });
-          res.writeHead(404, { 'Content-Type': 'text/html' });
-          console.log(error);
-        }
-
-        res.write(data);
-        res.end();
-      })
-      .listen(8080);
-
-    console.log(`Running at http://localhost:8080`);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-serve('/');
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
